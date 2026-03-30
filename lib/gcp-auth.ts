380dd2ce912f6b4c7831ff96256d367732fdde98
Carlos Gomes <gnosiq.admin@gmail.com>
2026-03-29 23:26:44 -0400

    feat: add GCP Workload Identity auth client

---
+++ lib/gcp-auth.ts
@@ -0,0 +1,13 @@
+import { getVercelOidcToken } from '@vercel/oidc';
+import { ExternalAccountClient } from 'google-auth-library';
+
+export function createGcpAuthClient() {
+  return ExternalAccountClient.fromJSON({
+    type: 'external_account',
+    audience: `//iam.googleapis.com/projects/${process.env.GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${process.env.GCP_WORKLOAD_IDENTITY_POOL_ID}/providers/${process.env.GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID}`,
+    subject_token_type: 'urn:ietf:params:oauth:token-type:jwt',
+    token_url: 'https://sts.googleapis.com/v1/token',
+    service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${process.env.GCP_SERVICE_ACCOUNT_EMAIL}:generateAccessToken`,
+    subject_token_supplier: { getSubjectToken: getVercelOidcToken },
+  });
+}
